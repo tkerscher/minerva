41f7f281888bd5c2d41fc38128bb2ae58cb36b08
tkerscher <88444139+tkerscher@users.noreply.github.com>
2022-06-13 02:06:43 +0200

    pushed version to 1.0.0

--- src/version.cpp
+++ src/version.cpp
@@ -1,5 +1,5 @@
 #include "minerva/version.hpp"
 
-const uint32_t minerva::VERSION_MAJOR = 0;
-const uint32_t minerva::VERSION_MINOR = 1;
+const uint32_t minerva::VERSION_MAJOR = 1;
+const uint32_t minerva::VERSION_MINOR = 0;
 const uint32_t minerva::VERSION_PATCH = 0;
